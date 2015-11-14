28285bfb3de8f42c0fdeb6a5ea463a51ce0f2a35
Aditya Varma <aditya.varma@zoomcar.com>
2015-11-15 00:47:31 +0530

    make code look prettier

--- Quora/Quora_hide_upvote_count.user.js
+++ Quora/Quora_hide_upvote_count.user.js
@@ -8,23 +8,24 @@
 // @grant        none
 // ==/UserScript==
 
-function hidecount(){
+function hideCount(){
     var span = document.getElementsByClassName("count");
     for(i = 0; i<span.length; i++){
         span[i].parentNode.removeChild(span[i]);
     }
 }
-function hideviewsandcredibility(){
+function hideViewsAndCredibility(){
     var div_element = document.getElementsByClassName("CredibilityFact");
     for(i=0; i < div_element.length; i++){
-        // div_element[i].parentNode.removeChild(div_element[i]); // uncomment this to remove credibility entirely
+        // div_element[i].parentNode.removeChild(div_element[i]);
+        // uncomment the above line to remove credibility entirely
         div_element[i].innerHTML = div_element[i].innerHTML.replace(/(^\d+)(\.\d+|)(k Views*| Views)/i, "");
     }
 }
-hidecount();
-hidecount();
-hideviewsandcredibility();
+hideCount();
+hideCount(); // for some reason the "follow" still shows count so calling it again.
+hideViewsAndCredibility();
 window.onscroll = function (e){
-    hidecount();
-    hideviewsandcredibility()
+    hideCount();
+    hideViewsAndCredibility()
 }
