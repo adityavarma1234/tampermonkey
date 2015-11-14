6f38ae9d67f6830d525f2e6ffa7d1ea493b66fe7
Aditya Varma <aditya.varma@zoomcar.com>
2015-11-14 05:34:37 +0530

    quora tamper monkey count script

---
+++ Quora hide count.user.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Quora hide count
+// @namespace    https://github.com/adityavarma1234/
+// @version      0.1
+// @description  Quora hide upvote count, comments count, followers count.
+// @author       Aditya Varma
+// @match        https://www.quora.com/*
+// @grant        none
+// ==/UserScript==
+
+function hidecount(){
+    var span = document.getElementsByClassName("count");
+    for(i = 0; i<span.length ; i++){
+        span[i].parentNode.removeChild(span[i]);
+    }
+}
+hidecount();
+window.onscroll = function (e){
+    hidecount();
+}
